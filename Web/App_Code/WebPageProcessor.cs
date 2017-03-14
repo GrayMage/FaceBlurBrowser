13a13290a09994d1e2c677ddbf92e8ab503baf70
GrayMage <graymage@users.noreply.github.com>
2017-03-15 01:41:54 +0500

    initial proxy

---
+++ Web/App_Code/WebPageProcessor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using HtmlAgilityPack;
+
+public class WebPageProcessor
+{
+    private readonly HtmlDocument _doc;
+    private readonly string _targetUrl;
+
+    public WebPageProcessor(string targetUrl)
+    {
+        _targetUrl = targetUrl;
+
+        var web = new HtmlWeb();
+
+        try
+        {
+            _doc = web.Load(_targetUrl);
+        }
+        catch (Exception)
+        {
+            _doc = null;
+        }
+    }
+
+    private static bool IsAbsoluteUri(string uri)
+    {
+        Uri result;
+        return Uri.TryCreate(uri, UriKind.Absolute, out result);
+    }
+
+    private void ProcessLinks()
+    {
+        if (!_doc.DocumentNode.HasChildNodes) return;
+
+        foreach (var link in _doc.DocumentNode.SelectNodes("//a[@href]"))
+        {
+            var attribute = link.Attributes["href"];
+
+            var url = attribute.Value;
+
+            attribute.Value = "/GetPage?q=" +
+                              Uri.EscapeUriString(IsAbsoluteUri(url) ? url : new Uri(new Uri(_targetUrl), url).ToString());
+        }
+    }
+
+    public string Process()
+    {
+        if (_doc == null)
+            return "<h2>No such site...</h2>";
+
+        ProcessLinks();
+
+        var sb = new StringBuilder();
+        var sw = new StringWriter(sb);
+
+        _doc.Save(sw);
+        return sw.ToString();
+    }
+}
