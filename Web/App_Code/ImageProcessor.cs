49e174b94565fbab8e863974cfdf5ffff18e5a23
GrayMage <graymage@users.noreply.github.com>
2017-03-15 22:13:55 +0500

    variable renamed

--- Web/App_Code/ImageProcessor.cs
+++ Web/App_Code/ImageProcessor.cs
@@ -16,13 +16,13 @@ public class ImageProcessor
     public byte[] Process()
     {
         var bitmap = new Bitmap(_image);
-        var fromImage = Graphics.FromImage(bitmap);
+        var graphics = Graphics.FromImage(bitmap);
         var image = new Image<Bgr, byte>(bitmap);
         foreach (var faceRect in new FaceDetector(bitmap).GetFaceRects())
         {
             var face = image.GetSubRect(faceRect);
             face = face.SmoothBlur(faceRect.Width / 5, faceRect.Height / 5, true);
-            fromImage.DrawImage(face.Bitmap, faceRect.Location);
+            graphics.DrawImage(face.Bitmap, faceRect.Location);
         }
 
         var ms = new MemoryStream();
