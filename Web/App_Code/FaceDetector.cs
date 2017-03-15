dfc1d1d10d1c1799ca3d4b5791bf5c2f45627db1
GrayMage <graymage@users.noreply.github.com>
2017-03-15 21:47:33 +0500

    Emgu face detection

---
+++ Web/App_Code/FaceDetector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+public class FaceDetector
+{
+    private readonly CascadeClassifier _cascadeClassifier;
+    private readonly Image<Gray, byte> _image;
+
+    public FaceDetector(Bitmap bitmap)
+    {
+        _image = new Image<Gray, byte>(bitmap);
+        var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+            "Bin\\haarcascade_frontalface_default.xml");
+        _cascadeClassifier = new CascadeClassifier(fileName);
+    }
+
+    public IEnumerable<Rectangle> GetFaceRects()
+    {
+        return _cascadeClassifier.DetectMultiScale(_image, 1.1, 10, Size.Empty);
+    }
+}
---
+++ Web/App_Code/ImageProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+public class ImageProcessor
+{
+    private readonly Image _image;
+
+    public ImageProcessor(byte[] imgData)
+    {
+        _image = Image.FromStream(new MemoryStream(imgData));
+    }
+
+    public byte[] Process()
+    {
+        var fromImage = Graphics.FromImage(_image);
+        foreach (var faceRect in new FaceDetector(new Bitmap(_image)).GetFaceRects())
+            fromImage.DrawRectangle(new Pen(Color.Red, 4), faceRect);
+
+        var ms = new MemoryStream();
+        _image.Save(ms, ImageFormat.Jpeg);
+        return ms.ToArray();
+    }
+}
